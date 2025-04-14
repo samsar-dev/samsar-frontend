bde488a6fb7511704df1a545bda03b1881a8859d
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-04-14 14:06:01 +0530

    fix: improve styling of save-listings

--- src/components/profile/SavedListings.tsx
+++ src/components/profile/SavedListings.tsx
@@ -1,12 +1,13 @@
+import { listingsAPI } from "@/api/listings.api";
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
+import { ListingAction, ListingCategory } from "@/types/enums";
+import type { Listing, ListingDetails } from "@/types/listings";
+import { motion } from "framer-motion";
 import React, { useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import type { Listing, ListingDetails } from "@/types/listings";
-import { ListingCategory, ListingAction } from "@/types/enums";
-import { motion } from "framer-motion";
-import { listingsAPI } from "@/api/listings.api";
-import { toast } from "react-toastify";
-import LoadingSpinner from "@/components/ui/LoadingSpinner";
 import { BsSend } from "react-icons/bs";
+import { MdDelete } from "react-icons/md";
+import { toast } from "react-toastify";
 
 interface ExtendedListing extends Omit<Listing, "details"> {
   seller?: {
@@ -89,7 +90,7 @@ const SavedListings: React.FC = () => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto bg-gray-50 dark:bg-gray-900 py-8">
+    <div className="max-w-4xl mx-auto bg-gray-50 dark:bg-transparent py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
           initial={{ opacity: 0 }}
@@ -99,10 +100,10 @@ const SavedListings: React.FC = () => {
         >
           <div className="">
             <h1 className="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
-              {t("profile.saved_listings")}
+              {t("Saved Listings")}
             </h1>
             <p className="mt-2 text-gray-600 dark:text-gray-400">
-              {t("profile.saved_listings_description")}
+              {t("View and manage the listings you've saved for later")}
             </p>
           </div>
 
@@ -113,74 +114,85 @@ const SavedListings: React.FC = () => {
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: -20 }}
-                className="bg-white/80 dark:bg-gray-800 flex rounded shadow-sm overflow-hidden border border-gray-200 dark:border-gray-700"
+                whileHover={{ y: -2 }}
+                transition={{ duration: 0.08, ease: "easeOut" }}
+                className="group bg-white/80 dark:bg-gray-800 flex items-end justify-between rounded overflow-hidden border border-gray-200 dark:border-gray-700 dark:hover:shadow hover:shadow-sm transition-all duration-100"
               >
-                <div className="relative">
-                  <div className="absolute top-3 left-3 z-10">
-                    <span
-                      className={`px-3 py-1 text-sm font-medium rounded-full ${
-                        item.listingAction === ListingAction.SELL
-                          ? "bg-blue-600 text-white"
-                          : "bg-green-600 text-white"
-                      }`}
-                    >
-                      {item.listingAction === ListingAction.SELL
-                        ? t("common.for_sale")
-                        : t("common.for_rent")}
-                    </span>
-                  </div>
-                  <div className="w-36 h-36 bg-gray-100 dark:bg-gray-700">
-                    {item.images && item.images.length > 0 && (
-                      <img
-                        src={item.images[0]}
-                        alt={item.title}
-                        className="inset-0 w-full h-full object-cover bg-white dark:bg-gray-800 bg-cover rounded-sm"
-                      />
-                    )}
-                  </div>
-                </div>
-                <div className="pt-4 pl-4 flex justify-between flex-col">
-                  <div>
-                    <div className="flex items-center gap-2 mb-3">
-                      <div className="w-6 h-6 rounded-full bg-gray-200 dark:bg-gray-700 overflow-hidden flex items-center justify-center">
-                        {item.seller?.profilePicture ? (
-                          <img
-                            src={item.seller.profilePicture}
-                            alt={item.seller.username}
-                            className="w-full h-full object-cover"
-                          />
-                        ) : (
-                          <div className="text-gray-400 text-sm text-center w-full">
-                            {item.seller?.username
-                              .charAt(0)
-                              .toLocaleUpperCase()}
-                          </div>
-                        )}
-                      </div>
-                      <span className="text-sm text-gray-600 dark:text-gray-400">
-                        {item.seller?.username || t("UserName")}
+                <div className="flex">
+                  <div className="relative overflow-visible">
+                    <div className="absolute top-2 left-2 z-40">
+                      <span
+                        className={`px-3 py-1 text-sm font-medium rounded-sm border opacity-0 group-hover:opacity-100 transition-all duration-150 ${
+                          item.listingAction.toLocaleLowerCase() ===
+                          ListingAction.SELL.toLocaleLowerCase()
+                            ? "bg-blue-600/90 text-white border-blue-700"
+                            : "bg-green-600/90 text-white border-emerald-700"
+                        }`}
+                      >
+                        {item.listingAction.toLocaleLowerCase() ===
+                        ListingAction.SELL.toLocaleLowerCase()
+                          ? t("Sell")
+                          : t("Rent")}
                       </span>
                     </div>
-                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white line-clamp-2">
-                      {item.title}
-                    </h3>
-                  </div>
-                  <div className="space-y-2">
-                    <div className="flex items-center text-sm text-gray-500 dark:text-gray-400">
-                      <span className="mr-2">{item.location}</span>
-                      {"area" in item.details && (
-                        <span>• {item.details.area} sq.m.</span>
+                    <div className="w-36 h-36 bg-gray-100 dark:bg-gray-700">
+                      {item.images && item.images.length > 0 && (
+                        <img
+                          src={item.images[0]}
+                          alt={item.title}
+                          className="inset-0 w-full h-full object-cover bg-white dark:bg-gray-800 bg-cover rounded-sm"
+                        />
                       )}
                     </div>
-                    <div className="flex items-center justify-between mt-4">
-                      <div className="text-xl font-bold text-blue-600 dark:text-blue-400">
-                        ${item.price.toLocaleString()}
+                  </div>
+                  <div className="pt-4 pl-4 flex justify-between flex-col">
+                    <div>
+                      <div className="flex items-center gap-2 mb-3">
+                        <div className="w-6 h-6 rounded-full bg-gray-200 dark:bg-gray-700 overflow-hidden flex items-center justify-center">
+                          {item.seller?.profilePicture ? (
+                            <img
+                              src={item.seller.profilePicture}
+                              alt={item.seller.username}
+                              className="w-full h-full object-cover"
+                            />
+                          ) : (
+                            <div className="text-gray-400 text-sm text-center w-full">
+                              {item.seller?.username
+                                .charAt(0)
+                                .toLocaleUpperCase()}
+                            </div>
+                          )}
+                        </div>
+                        <span className="text-sm text-gray-600 dark:text-gray-400">
+                          {item.seller?.username || t("UserName")}
+                        </span>
                       </div>
-                      <button className="p-2 flex items-center justify-center text-gray-600 dark:text-gray-400 rounded-full border-2 border-gray-200 dark:border-gray-700 hover:text-blue-600 dark:hover:text-blue-400 transition-colors">
-                        <BsSend className="w-5 h-5 " />
-                      </button>
+                      <h3 className="text-lg font-semibold text-gray-900 dark:text-white line-clamp-2">
+                        {item.title}
+                      </h3>
+                    </div>
+                    <div className="space-y-2">
+                      <div className="flex items-center text-sm text-gray-500 dark:text-gray-400">
+                        <span className="mr-2">{item.location}</span>
+                        {"area" in item.details && (
+                          <span>• {item.details.area} sq.m.</span>
+                        )}
+                      </div>
+                      <div className="flex items-center justify-between mt-4">
+                        <div className="text-xl font-bold text-blue-600 dark:text-blue-400">
+                          ${item.price.toLocaleString()}
+                        </div>
+                      </div>
                     </div>
                   </div>
+                </div>
+                <div className=" h-full flex translate-x-28 border-gray-200 dark:border-gray-700 opacity-0 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-500">
+                  <button className="p-2 mx-2 my-1 flex items-center justify-center text-gray-600 dark:text-gray-400 rounded-full border-2 border-gray-200 dark:border-gray-700 hover:text-blue-700 hover:bg-blue-600/50 hover:border-blue-600/30 dark:hover:border-blue-600/20 dark:hover:text-blue-400 transition-colors duration-300">
+                    <BsSend className="w-5 h-5 " />
+                  </button>
+                  <button className="p-2 mx-2 my-1 flex items-center justify-center text-gray-600 dark:text-gray-400 rounded-full border-2 border-gray-200 dark:border-gray-700 hover:text-red-700 hover:bg-red-600/50 hover:border-red-600/30 dark:hover:border-red-600/20 dark:hover:text-red-400 transition-colors duration-300">
+                    <MdDelete className="w-5 h-5 " />
+                  </button>
                 </div>
               </motion.div>
             ))}
