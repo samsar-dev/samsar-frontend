a5f922ef90ee18d5f4a896a536c5b577a447e689
Sultan00786 <treeloversultan0987@gmail.com>
2025-05-12 19:36:33 +0530

    feat: new setting field added in user and auth user type

--- src/types/user.ts
+++ src/types/user.ts
@@ -13,6 +13,13 @@ export interface User {
   role: "user" | "admin";
   createdAt: string;
   updatedAt: string;
+
+  allowMessaging?: boolean;
+  listingNotifications?: boolean;
+  messageNotifications?: boolean;
+  showEmail?: boolean;
+  showOnlineStatus?: boolean;
+  showPhoneNumber?: boolean;
 }
 
 export interface UserProfile extends User {
