0e79fe7ab1aefd7fd02d13cae90b56def87ad6d7
samsar <samsar@example.com>
2025-07-24 13:10:05 +0200

    ✅ updated something

--- postcss.config.js
+++ postcss.config.js
@@ -1,49 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? {
-      '@fullhuman/postcss-purgecss': {
-        content: [
-          './src/**/*.{js,jsx,ts,tsx,vue}',
-          './public/**/*.html',
-          './index.html',
-        ],
-        safelist: [
-          'html',
-          'body',
-          'dark',
-          /^bg-/,
-          /^text-/,
-          /^border-/,
-          /^hover:/,
-          /^focus:/,
-          /^disabled:/,
-          /^dark:/,
-          /^min-h-/,
-          /^w-/,
-          /^h-/,
-          /^max-w-/,
-          /^rounded-/,
-          /^shadow-/,
-          /^flex/,
-          /^grid/,
-          /^space-/,
-          /^p-/,
-          /^m-/,
-          /^px-/,
-          /^py-/,
-          /^pt-/,
-          /^pb-/,
-          /^pl-/,
-          /^pr-/,
-        ],
-        keyframes: true,
-        variables: true,
-        fontFace: true,
-      },
-    } : {}),
   },
 };
