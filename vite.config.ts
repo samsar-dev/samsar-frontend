4be9e5c6e7d35be58053cee7c16aba609b5e4c9e
samsar <samsar@example.com>
2025-07-25 18:30:30 +0200

    ✅ updated something

--- vite.config.ts
+++ vite.config.ts
@@ -138,6 +138,7 @@ export default defineConfig(({ mode, command }) => {
         '@types': path.resolve(__dirname, "src/types"),
         '@utils': path.resolve(__dirname, "src/utils"),
       },
+      dedupe: ["react", "react-dom"],
     },
 
     server: {
