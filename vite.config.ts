d84a1e7518627de55a993466bff89d9975f03949
samsar <samsar@example.com>
2025-08-07 11:46:45 +0200

    ✅ updated something

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import commonjs from '@rollup/plugin-commonjs';
 import Inspect from 'vite-plugin-inspect';
 import path from "path";
 import { fileURLToPath } from "url";
@@ -45,6 +46,7 @@ export default defineConfig(({ mode, command }) => {
     },
 
     plugins: [
+      commonjs(),
       react({
         // Let Vite handle React DevTools
         jsxImportSource: 'react',
